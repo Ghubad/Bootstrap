1d9edeb1e0bd14b8e13e9dc7a5bf4ccd946026a1
Pooja <pooja.cp@gmail.com>
2017-09-24 14:42:13 +0530

    Nav pills, Nav tabs, Accordion, Well, Accordion, Carousel added

--- js/site.js
+++ js/site.js
@@ -38,4 +38,29 @@
         $pickButton.text(reason);
     });
 
+    var $sentDialog = $("#sentDialog");
+    $("#contactForm").on("submit", function () {
+        $sentDialog.modal('show');
+        return false;
+    })
+
+    var $sentAlert = $("#sentAlert");
+    $sentDialog.on("hidden.bs.modal", function () {
+        $sentAlert.show();
+    })
+
+    $sentAlert.on("close.bs.alert", function() {
+        $sentAlert.hide();
+        return false;
+    })
+
+    $("#contactForm [input type=submit]").tooltip({
+        delay: {
+            show: 500,
+            hide:0
+        }
+    });
+
+    $("#theCarousel").carousel();
+
 })();
